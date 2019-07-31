aea2fc2496f7ef67362245c96be263713a0b6814
Rouzip <1226015390@qq.com>
2019-07-31 21:25:05 +0800

    try shelljs

---
+++ README.md
@@ -0,0 +1,13 @@
+# some ideas
+
+- [x] get repo name
+- [ ] get repo data from `Github` (place in the `/tmp`)
+- [ ] execute `hugo` in the child shell to execute this mission
+- [ ] stop `nginx`
+- [ ] `mv` the html and imagine to the right folder
+- [ ] start `nginx`
+
+## plan
+
+1. try shell.js to execute bash
+2. hugo generate html
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import http from "http";
 import crypto from "crypto";
 
+import shell from "shelljs";
+import R from "ramda";
+
 const PORT: number = Number(process.env.port) || 5555; // server port
 const KEY: string = process.env.secret || ""; // write your secret key here
+const BLOG: string = process.env.place || ""; // blog place
 
 /**
  * judge if two buffer string same
@@ -21,7 +25,6 @@ function eq(a: Buffer, b: Buffer): boolean {
   return res === 0;
 }
 
-
 /**
  * check data in buffer
  * @param sign get the data from request header
@@ -31,29 +34,39 @@ function bufferEqual(sign: any, data: any): boolean {
   return eq(
     Buffer.from(sign),
     Buffer.from(
-      "sha1=".concat(crypto
-        .createHmac("sha1", KEY)
-        .update(data)
-        .digest("hex"))
+      "sha1=".concat(
+        crypto
+          .createHmac("sha1", KEY)
+          .update(data)
+          .digest("hex")
+      )
     )
   );
 }
 
-
 // create server
 http
-  .createServer(function (req, res) {
-    req.on("data", async function (data: Buffer) {
+  .createServer(function(req, res) {
+    req.on("data", async function(data: Buffer) {
       try {
         if (bufferEqual(req.headers["x-hub-signature"], data)) {
-          let tmp = JSON.parse(data.toString());
-          console.log(tmp['repository']['clone_url']);
-        }
-        else {
+          let gitName = JSON.parse(data.toString())["repository"]["clone_url"];
+          let repoName = JSON.parse(data.toString())["repository"]["name"];
+          shell.cd("/tmp");
+          shell.exec("git clone " + gitName);
+          shell.mv("/tmp/" + repoName + "/md/*", BLOG + "/content/post"); // move the md posts into content post
+          shell.mv("/tmp/" + repoName + "/img/*", BLOG + "/static");
+
+          shell.exec("systemctl stop nginx.service");
+          shell.cd(BLOG);
+          shell.rm("-rf", "public");
+          shell.exec("hugo");
+          shell.exec("systemctl start nginx.service");
+        } else {
           // FIXME: need to log in the server
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     });
     res.end();
